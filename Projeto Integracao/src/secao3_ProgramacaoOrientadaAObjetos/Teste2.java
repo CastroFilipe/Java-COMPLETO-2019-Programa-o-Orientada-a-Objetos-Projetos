a1262d9bb3f8575c6096f14e50be2f02bdc5cd83
Filipe Castro <filipe.couto.10@gmail.com>
2019-04-05 15:48:13 -0300

    Novos testes

---
+++ Projeto Integracao/src/secao3_ProgramacaoOrientadaAObjetos/Teste2.java
@@ -0,0 +1,9 @@
+package secao3_ProgramacaoOrientadaAObjetos;
+
+public class Teste2 {
+
+	public static void main(String[] args) {
+		System.out.print("Funcionou");
+	}
+
+}
---
+++ Projeto Integracao/src/secao3_ProgramacaoOrientadaAObjetos/teste.java
@@ -0,0 +1,10 @@
+package secao3_ProgramacaoOrientadaAObjetos;
+
+public class teste {
+
+	public static void main(String[] args) {
+		System.out.print("Teste2");
+
+	}
+
+}
